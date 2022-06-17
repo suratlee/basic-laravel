ef2d3c950eb8d44365812fef654c07e644aa577b
suratlee <suratlee@gmail.com>
2022-06-17 15:12:32 +0700

    Route coding

--- basic-laravel/routes/web.php
+++ basic-laravel/routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/member',function(){
+    return view('member.index');
+});
+
+Route::get('/admin',function(){
+    return view('admin.index');
+});
+
+Route::get('/dashboard',function(){
+    return view('admin.dashboard.page');
+});
